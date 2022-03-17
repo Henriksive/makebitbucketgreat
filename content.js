5c7e93af5d7b15bb1082fef1aa06e646d87b54e9
Henrik Sivertsgård <henriksive@gmail.com>
2022-03-17 13:59:33 +0100

    Alltid fjern banner, viktige meldinger bør komme en annen plass

--- content.js
+++ content.js
@@ -1,11 +1,4 @@
 var banner = document.getElementsByClassName("announcement-banner-content")[0];
-if (
-  banner.innerHTML.startsWith("<br>Ta kontakt med") ||
-  banner.innerHTML.startsWith("<p>Ta kontakt med") ||
-  banner.innerHTML.startsWith("Ta kontakt med")
-) {
-  var bannerContainer = document.getElementById(
-    "bitbucket-announcement-banner"
-  );
-  bannerContainer.style.display = "none";
-}
+
+var bannerContainer = document.getElementById("bitbucket-announcement-banner");
+bannerContainer.style.display = "none";
