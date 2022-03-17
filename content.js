f653e50104d35b7c808f9283a93a9f1506e8fd19
Yrjan Fraschetti <yrjanaff@gmail.com>
2022-03-17 22:05:50 +0100

    Deler opp i chrome og firefox-mapper. Legger til extension for firefox.

--- content.js
+++
@@ -1,4 +0,0 @@
-var banner = document.getElementsByClassName("announcement-banner-content")[0];
-
-var bannerContainer = document.getElementById("bitbucket-announcement-banner");
-bannerContainer.style.display = "none";
