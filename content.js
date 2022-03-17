dfa08dd45bd33210c3bd9a6f7672cda595b1c026
Henrik Sivertsgård <henriksive@gmail.com>
2022-03-17 13:03:27 +0100

    poc

---
+++ content.js
@@ -0,0 +1,8 @@
+// var item = document.getElementsByClassName(
+//   "status-button approve css-e0xx2c"
+// )[0];
+// item.innerHTML = "Approve pull request";
+
+// item.addEventListener("click", () => {
+//   item.innerHTML = "Click to unapprove";
+// });
